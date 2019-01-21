51748bfa20802849afa32cb7f1dd9a1944daf8b1
Debbie O'Brien <dobrien@todopatterson.com>
2019-01-21 17:50:17 +0100

    fix error

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,6 +68,7 @@ module.exports = Object.assign(routerBase, {
     /*
     ** You can extend webpack config here
     */
+    mode: postcss,
     postcss: {
       plugins: {
         tailwindcss: path.resolve(__dirname, './tailwind.js')
